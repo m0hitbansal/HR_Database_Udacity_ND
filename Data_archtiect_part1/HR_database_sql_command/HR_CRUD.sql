427e0889b94d84c8d5d0d3002ac95029e15294e8
mohit.bansal <mohit.bansal@payu.in>
2022-11-23 11:21:38 +0530

    added course

---
+++ Data_archtiect_part1/HR_database_sql_command/HR_CRUD.sql
@@ -0,0 +1,48 @@
+/* Question 1: Return a list of employees with Job Titles and Department Names */
+
+select emp.employee_name,j.job_title,d.dep_name 
+from Employment as e
+join employee as emp on e.employee_id=emp.employee_id
+join job as j on e.job_id=j.job_id
+join department as d on e.dep_id=d.dep_id;
+
+    
+/* Question 2: Insert Web Programmer as a new job title */
+
+insert into Job (job_title)
+    values ('Web Programmer');
+    
+    
+/* Question 3: Correct the job title from web programmer to web developer */
+
+update job
+    SET job_title = 'Web Developer'
+    WHERE job_title = 'Web Programmer';
+    
+    
+/* Question 4: Delete the job title Web Developer from the database */
+
+delete from job
+    WHERE job_title = 'Web Developer';
+    
+    
+/* Question 5: How many employees are in each department? */
+
+select d.dep_name, count(employee_id)
+from Employment as e
+join Department as d on e.dep_id=d.dep_id
+where e.end_date >CURRENT_DATE
+group by d.dep_name;
+        
+/* Question 6: Write a query that returns current and past jobs (include 
+   employee name, job title, department, manager name, start and end date 
+   for position) for employee Toni Lembeck. */
+   
+select emp.employee_name,j.job_title,d.dep_name,
+    (select employee_name from employee where employee_id=e.manager_id),
+    e.start_date,e.end_date
+from Employment as e
+join employee as emp on emp.employee_id=e.employee_id
+join job as j on e.job_id=j.job_id
+join department as d on d.dep_id=e.dep_id
+where emp.employee_name='Toni Lembeck';
--- README.md
+++
@@ -1,3 +0,0 @@
-# HR_Database_Udacity_ND
-
-In this project, I have designed, built, and populated a database for the Human Resources (HR) Department at the imaginary Tech ABC Corp, a video game company. This project will start with a request from the HR Manager. From there, I had to design a database using the foundational principals of data architecture that is best suited to the department's needs. This project is important, as it is a scaled-down simulation of the kind of real-world assignments data architects work on every day.
